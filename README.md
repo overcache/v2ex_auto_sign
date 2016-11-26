93e168c4f5ba369653f72a382adf2a92938deedc
icymind <icyminnd@gmail.com>
2016-11-26 11:38:00 +0800

    add multi account support

---
+++ README.md
@@ -0,0 +1,8 @@
+# V2ex-auto-signin
+
+## installation
+
+```bash
+sudo apt-get install ruby-dev g++ zlib1g-dev
+sudo gem install mechanize mail
+```
--- v2ex.rb
+++ v2ex.rb
@@ -2,13 +2,14 @@
 require "mechanize"
 require "mail"
 
-ACCOUNT = "EchoWhale"
-PASSWORD = "xxxxx"
-MAIL = "mail@qq.com"
-MAIL_PWD = "xxxx"
-TO_MAIL = "some@gmail.com"
-mail_text = ""
-success = true
+ACCOUNTS= [
+  ["your-v2-account", "your-password-for-v2-account"],
+]
+# ACCOUNT = "EchoWhale"
+# PASSWORD = "your-password-for-v2-account"
+MAIL = "your-qq-mail@qq.com"
+MAIL_PWD = "password-of-your-qq-mail@qq.com"
+TO_MAIL = "your-mail-for-receive-notification@gmail.com"
 
 Mail.defaults do
   delivery_method :smtp, {
@@ -25,53 +26,61 @@ end
 agent = Mechanize.new
 agent.user_agent_alias = "Mac Safari"
 
-begin
-  page = agent.get("http://www.v2ex.com/signin")
-  login_form = page.form_with(:action => "/signin")
-  login_form.field_with(:type => "text").value = ACCOUNT
-  login_form.field_with(:type => "password").value = PASSWORD
-  # login_form.u = ACCOUNT
-  # login_form.p = PASSWORD
-  login_rsp = agent.submit(login_form)
+for account in ACCOUNTS
 
-  if login_rsp.uri.path == "/"
-    puts "登陆成功"
-    mission_page = agent.get("/mission/daily")
-    input_element = mission_page.search("input").select do |node|
-      !node.attributes["class"].nil? && node.attributes["class"].value == "super normal button"
-    end
-    href = input_element.first.attributes["onclick"].value.split("'")[1]
-    if href == "/balance"
-      puts "今日已经领取"
-    else
-      finish_page = agent.get(href)
-      message = finish_page.at("//div[@class='message']/text()").to_s
-      success = message.include?("已成功领取每日登录奖励") ? true : false
-      if success
-        puts "领取成功"
-      else
-        mail_text = "领取失败"
-        puts mail_text
+  user_name = account.first
+  user_pwd = acount.last
+
+  begin
+    mail_text = ""
+    success = true
+    page = agent.get("http://www.v2ex.com/signin")
+    login_form = page.form_with(:action => "/signin")
+    login_form.field_with(:type => "text").value = user_name
+    login_form.field_with(:type => "password").value = user_pwd
+    # login_form.u = ACCOUNT
+    # login_form.p = PASSWORD
+    login_rsp = agent.submit(login_form)
+
+    if login_rsp.uri.path == "/"
+      puts "登陆成功"
+      mission_page = agent.get("/mission/daily")
+      input_element = mission_page.search("input").select do |node|
+        !node.attributes["class"].nil? && node.attributes["class"].value == "super normal button"
       end
+      href = input_element.first.attributes["onclick"].value.split("'")[1]
+      if href == "/balance"
+        puts "今日已经领取"
+      else
+        finish_page = agent.get(href)
+        message = finish_page.at("//div[@class='message']/text()").to_s
+        success = message.include?("已成功领取每日登录奖励") ? true : false
+        if success
+          puts "领取成功"
+        else
+          mail_text = "领取失败"
+          puts mail_text
+        end
+      end
+    else
+      success = false
+      mail_text = "V2EX登录失败"
+      puts mail_text
     end
-  else
+  rescue => e
     success = false
-    mail_text = "V2EX登录失败"
+    mail_text = "未知错误: #{e}"
     puts mail_text
   end
-rescue => e
-  success = false
-  mail_text = "未知错误: #{e}"
-  puts mail_text
-end
 
-unless success
-  mail = Mail.new do
-    from "#{MAIL}"
-    to  "#{TO_MAIL}"
-    subject "天啦噜！V2EX没能自动签到"
-    body  (mail_text << "\n用户: #{ACCOUNT}")
+  unless success
+    mail = Mail.new do
+      from "#{MAIL}"
+      to  "#{TO_MAIL}"
+      subject "天啦噜！V2EX没能自动签到"
+      body  (mail_text << "\n用户: #{user_name}")
+    end
+    mail.charset = "utf-8"
+    mail.deliver
   end
-  mail.charset = "utf-8"
-  mail.deliver
 end
